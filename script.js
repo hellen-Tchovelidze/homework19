edb82f9cc2d986b124d520e82d9d0974712b1bd2
hellen-Tchovelidze <elenechovelidze@gmail.com>
2024-10-31 21:12:20 +0400

    js

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
-let info = prompt("Enter your information:")
-let ul = document.getElementById("ul");
-let addbutton = document.getElementById("add_button");
-let remove = document.getElementById("remove");
-
-function InfoControl(info) {
-    if (info === null || info.trim() === "") {
-        return alert("You must enter some information!")
-    } else {
-        
-    }
-}
-
+let addbutton = document.getElementById("button")
+let ul = document.querySelector("ul")
 
 addbutton.addEventListener("click", () => {
-  let li = document.createElement("li");
-li.innerText = info
-  ul.appendChild(li);
-});
-
-remove.addEventListener("click", () => {
-  if (ul.children.length > 0) {
-    ul.firstChild.remove();
-  } else {
-    alert("ERROR");
-  }
-});
+    let info = prompt ("please enter your information")
+    if (info) { let li = document.createElement("li")
+        li.innerText = info
+    ul.appendChild(li)
+    let delateinfo = document.createElement("button")
+    li.appendChild(delateinfo)
+    delateinfo.innerText = "delate"
+    delateinfo.addEventListener("click", () => {
+        ul.removeChild(li)
+    })
+        
+    } else {
+        alert("please, add your information")
+    }
+})
